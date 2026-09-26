bef153bf2b2715360060c805f02b48b131855a2f
Rushabh <161584263+Rushabh-00@users.noreply.github.com>
2026-09-26 21:53:01 +0530

    cleanup: remove unused home quick action composition local

--- OpenRealmLauncher/src/main/java/dev/openrealm/launcher/ui/screens/content/home/HomeCards.kt
+++ OpenRealmLauncher/src/main/java/dev/openrealm/launcher/ui/screens/content/home/HomeCards.kt
@@ -23,12 +23,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.clickable
@@ -36,7 +34,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import com.movtery.cardgrid.model.CardLimits
 import com.movtery.cardgrid.model.CardType
 import dev.openrealm.launcher.BuildConfig
@@ -56,8 +53,6 @@ data class HomeQuickActionActions(
     val downloads: () -> Unit,
     val mods: () -> Unit,
 )
-
-val LocalHomeQuickActionActions = staticCompositionLocalOf<HomeQuickActionActions?> { null }
 
 private data class HomeQuickAction(
     val id: String,
